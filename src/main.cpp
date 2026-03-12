29034e3104e801f81c60cbcd083a1c336298d297
jherna3 <jherna3@uw.edu>
2026-03-12 14:38:42 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ void complete_operation(const std::string& user_input) noexcept {
   if (geq_4 && user_input.substr(0, 4) == "echo") {
     std::cout << user_input.substr(5);
   } else if (geq_4 && user.substr(0, 4) == "type") {
-    const bool is_type = is_input_shell_type(user.substr(5));
+    const bool is_type = is_input_shell_type(user_input.substr(5));
     if (is_type) {
       std::cout << user_input.substr(0, 4) << " is a shell builtin";
     } else {
