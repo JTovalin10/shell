9d33c2bfeafd615c1edb7bf3048123ac6a6892a7
jherna3 <jherna3@uw.edu>
2026-03-12 14:05:12 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,16 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  std::string user_input;
+  while (true) {
+    std::cout << "$ ";
+    std::cin >> user_input;
+    if (user_input.size() >= 0) {
+      std::cout << user_input + ": command not found";
+    }
+    user_input.clear()
+  }
   std::cout << "$ ";
   std::string user_input;
-  std::cin >> user_input;
-  if (user_input.size() >= 0) {
-    std::cout << user_input + ": command not found";
-  }
+
 }
