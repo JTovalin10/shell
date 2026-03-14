33de669241be7ac234404d13eb5bcb2481297095
JTovalin <justinhernlin@icloud.com>
2026-03-13 22:04:46 -0700

    set up for parse_args

--- src/ShellHelper/ShellHelper.cpp
+++ src/ShellHelper/ShellHelper.cpp
@@ -2,7 +2,19 @@
 
 #include "Commands/BuiltInCommand.hpp"
 
+enum class STRING_STATE { NORMAL, SINGLE_QUOTE, DOUBLE_QUOTE, BACKSLASH };
+
 namespace Slime {
+
+// we remove constness so that we can do parsed_args[i] =
+// std::move(user_args[j])
+std::vector<std::string> parse_args(std::vector<std::string>& user_args) {
+  STRING_STATE state = STRING_STATE::NORMAL;
+  // reserver the space for the size so that we dont need to resize
+  std::vector<std::string> parsed_args(user_args.size());
+  // on normal whenever we reach a " ", append it to the list
+  return parsed_args;
+}
 
 bool is_input_shell_type(const std::string& input) noexcept {
   return CommandRegistry::IsBuiltIn(input);
--- src/ShellHelper/ShellHelper.hpp
+++ src/ShellHelper/ShellHelper.hpp
@@ -4,8 +4,7 @@
 #include <vector>
 
 namespace Slime {
-std::vector<std::string> parse_args(
-    const std::vector<std::string>& user_args);
+std::vector<std::string> parse_args(std::vector<std::string>& user_args);
 
 bool is_input_shell_type(const std::string& input) noexcept;
 };  // namespace Slime
